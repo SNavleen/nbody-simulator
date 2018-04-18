7b01a6fdfb673f195bb18fd381cdc164182a5b2d
Randa <randamohsen@hotmail.ca>
2018-04-17 22:14:41 -0400

    added acceleration

--- scrap.cpp
+++ scrap.cpp
@@ -25,8 +25,11 @@ h = t[i+1] = i[i];
 /* particle q properties */
 q.speed[i+1] = q.speed[i] + h * q.velocity[i];
 q.velocity[i+1] = q.velocity[i] + h * q.F[i] / q.mass; // velocity = speed`
-q.acceleration = ?; // acceleration = speed``
 
+// the following is not from the slides, i just guessed it
+q.acceleration[i+1] = (q.velocity[i+1] - q.velocity[i]) / (t[i+1] - t[i]); // acceleration = speed``
+
+// this formula is wacky and from slide 9
 // other particle k exerts force on current particle q
 q.force[i] = (-G) * (q.mass * k.mass) / (distance between q and k) * (q.speed - k.speed);
 
@@ -42,12 +45,13 @@ for (x=0; x<N; x++){
     // k = y-th particle
 
     if (k!=q){
-      // compute f(qk,i) -- see slide because i'm confused
+      // compute f(qk,i) -- see slide 10 because i'm confused
       q.force[i] = q.force[i] + f(qk,i)
 
     }
   }
 }
+
 // reduced alogirthm
 for (x=0; x<N; x++){
   // q = x-th particle
@@ -59,7 +63,7 @@ for(y=0; y<N; y++){
   // k = y-th particle
 
   if (k!=q){
-    // compute f(qk,i) -- see slide because i'm confused
+    // compute f(qk,i) -- see slide 10 because i'm confused
     q.force[i] = q.force[i] + f(qk,i)
     k.force[i] = k.force[i] + f(qk,i)
 
