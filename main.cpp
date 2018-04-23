04c0e2ca82901449a58bfee1be8eb008cef5cce9
Navleen <navleen95@gmail.com>
2018-04-23 12:48:44 -0400

    sync

--- main.cpp
+++ main.cpp
@@ -5,54 +5,374 @@
 
 #include <mpi.h>
 #include <math.h>
+#include <time.h> /* time */
 
 #include "vector3d.h"
 #include "savebmp.h"
 #include "properties.h"
 
 #define epsilon 0.000000000000000222
+#define particleRadius 20
 
 using namespace std;
 
-int main(int argc, char* argv[]){
 
-	if( argc != 10){
-		printf("Usage: %s numParticlesLight numParticleMedium numParticleHeavy numSteps subSteps timeSubStep imageWidth imageHeight imageFilenamePrex\n", argv[0]);
-		exit(EXIT_FAILURE);
-	}
+typedef struct {
+  double const velocityMin;
+  double const velocityMax;
+  double const massMin;
+  double const massMax;
+  vec3 const   colour;
+} ParticleProperties;
 
-	MPI_Init(&argc,&argv);
+typedef struct {
+  vec3   position;
+  vec3   velocity;
+  vec3   force;
+  vec3   colour;
+  double mass;
+  int    radius;
+} Particle;
 
-	int p, my_rank;
 
-	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &p);
+void particlesInit(int                startIndex,
+                   int                endIndex,
+                   int                width,
+                   int                height,
+                   Particle          *particles,
+                   ParticleProperties particleProperties);
+void updateParticles(int       numParticles,
+                     int       h,
+                     Particle *oldParticles,
+                     Particle *newParticles);
+void imgInit(int            h,
+             int            w,
+             unsigned char *img);
+void imgUpdate(Particle const *particles,
+               int             numParticles,
+               unsigned char  *img,
+               int             height,
+               int             width);
+void putpixel(int            x,
+              int            y,
+              unsigned char *img,
+              int            width,
+              vec3           colour);
 
-	//variables
-	int numParticlesLight = 0;
-	int numParticleMedium = 0;
-	int numParticleHeavy = 0;
+const float G = 6.67300E-11;
 
-	int numSteps = 0;
-	int subSteps = 0;
-	double timeSubStep;
+// MPI variables
+int p, my_rank;
+// Status of a MPI_Recv
+MPI_Status status;
+// Request of a MPI_Isend
+MPI_Request request;
+/* New Datatype for vec3 */
+MPI_Datatype mpi_vec3, mpi_Particle, mpiold_Particle[4];
+MPI_Aint     offsetsParticle[4], addrParticle[4];
 
-	int width, height;
+// Input variables
+int numParticlesLight, numParticlesMedium, numParticlesHeavy, numSteps, subSteps,
+    width, height;
+double timeSubStep;
 
-	unsigned char* image;
+// Particle Informations
+int totalParticles;
 
-	//root node stuff goes here
-	if(my_rank == 0){
-		//almost done, just save the image
-		saveBMP(argv[9], image, width, height);
-	}
-	//all other nodes do this
-	else{
+ParticleProperties lightProperties =
+{ velocityLightMin, velocityLightMax, massLightMin, massLightMin, colourLight };
+ParticleProperties mediumProperties =
+{ velocityMediumMin, velocityMediumMax, massMediumMin, massMediumMin,
+  colourMedium };
+ParticleProperties heavyProperties =
+{ velocityHeavyMin, velocityHeavyMax, massHeavyMin, massHeavyMin, colourHeavy };
 
-	}
+Particle *particlesOld, *particlesNew, *particlesNewLocal;
 
-	free(image);
+int main(int argc, char *argv[]) {
+  if (argc != 10) {
+    printf(
+      "Usage: %s numParticlesLight numParticleMedium numParticleHeavy numSteps subSteps timeSubStep imageWidth imageHeight imageFilenamePrex\n",
+      argv[0]);
+  }
+  srand48(time(NULL));
 
-	MPI_Finalize();
-	return 0;
+
+  MPI_Init(&argc, &argv);
+
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &p);
+
+  // New data type for vec3
+  MPI_Type_contiguous(3, MPI_DOUBLE, &mpi_vec3);
+  MPI_Type_commit(&mpi_vec3);
+
+  // variables
+  numParticlesLight  = atoi(argv[1]);
+  numParticlesMedium = atoi(argv[2]);
+  numParticlesHeavy  = atoi(argv[3]);
+
+  numSteps    = atoi(argv[4]);
+  subSteps    = atoi(argv[5]);
+  timeSubStep = atof(argv[6]);
+
+  width  = atoi(argv[7]);
+  height = atoi(argv[8]);
+
+  unsigned char *image = (unsigned char *)malloc(height * width * 3);
+
+  totalParticles = numParticlesLight + numParticlesMedium + numParticlesHeavy;
+
+  particlesOld      = (Particle *)malloc(totalParticles * sizeof(Particle));
+  particlesNew      = (Particle *)malloc(totalParticles * sizeof(Particle));
+  particlesNewLocal = (Particle *)malloc((totalParticles / p) * sizeof(Particle));
+
+  // New data type for Particle struct
+  int blockLenParticle[4];
+  MPI_Get_address(particlesOld,          &addrParticle[0]);
+  MPI_Get_address(&particlesOld->colour, &addrParticle[1]);
+  MPI_Get_address(&particlesOld->mass,   &addrParticle[2]);
+  MPI_Get_address(&particlesOld->radius, &addrParticle[3]);
+  blockLenParticle[0] = 4;
+  blockLenParticle[1] = 1;
+  blockLenParticle[2] = 1;
+  blockLenParticle[3] = 1;
+  offsetsParticle[0]  = 0;
+  offsetsParticle[1]  = addrParticle[2] - addrParticle[0];
+  offsetsParticle[2]  = addrParticle[3] - addrParticle[0];
+  offsetsParticle[3]  = sizeof(Particle);
+  mpiold_Particle[0]  = mpi_vec3;
+  mpiold_Particle[1]  = MPI_DOUBLE;
+  mpiold_Particle[2]  = MPI_INT;
+  mpiold_Particle[3]  = MPI_UB;
+  MPI_Type_struct(4,
+                  blockLenParticle,
+                  offsetsParticle,
+                  mpiold_Particle,
+                  &mpi_Particle);
+  MPI_Type_commit(&mpi_Particle);
+
+
+  // root node stuff goes here
+  if (my_rank == 0) {
+    // printf("Rank %d - totalParticles: %d\n", my_rank, totalParticles);
+    particlesInit(0, numParticlesLight,
+                  width,
+                  height,
+                  particlesOld,
+                  lightProperties);
+    particlesInit(numParticlesLight, numParticlesLight + numParticlesMedium,
+                  width,
+                  height,
+                  particlesOld,
+                  mediumProperties);
+    particlesInit(numParticlesLight + numParticlesMedium, totalParticles,
+                  width,
+                  height,
+                  particlesOld,
+                  heavyProperties);
+    for (int dest = 0; dest < p; dest++) {
+      MPI_Isend(&totalParticles,
+                1,
+                MPI_INT,
+                dest,
+                0,
+                MPI_COMM_WORLD,
+                &request);
+    }
+  } else {
+    MPI_Recv(&totalParticles, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+  }
+  for (int i = 0; i < numSteps * subSteps; i++) {
+    MPI_Bcast(&particlesOld[0], totalParticles, mpi_Particle, 0, MPI_COMM_WORLD);
+    // printf("After Rank %d (particlesOld position) - x: %f, y: %f, z: %f\n",
+    //        my_rank,
+    //        particlesOld[0].position.x,
+    //        particlesOld[0].position.y,
+    //        particlesOld[0].position.z);
+    // printf("After Rank %d (particlesOld velocity) - x: %f, y: %f, z: %f\n",
+    //        my_rank,
+    //        particlesOld[0].velocity.x,
+    //        particlesOld[0].velocity.y,
+    //        particlesOld[0].velocity.z);
+    //  printf("After Rank %d (particlesOld force) - x: %f, y: %f, z: %f\n",
+    //         my_rank,
+    //         particlesOld[0].force.x,
+    //         particlesOld[0].force.y,
+    //         particlesOld[0].force.z);
+    // printf("After Rank %d (particlesOld colour) - x: %f, y: %f, z: %f\n",
+    //        my_rank,
+    //        particlesOld[0].colour.x,
+    //        particlesOld[0].colour.y,
+    //        particlesOld[0].colour.z);
+    // printf("After Rank %d (particlesOld) - mass: %f, radius: %d\n",
+    //                   my_rank,
+    //                   particlesOld[0].mass,
+    //                   particlesOld[0].radius);
+
+    updateParticles(totalParticles,
+                    timeSubStep,
+                    particlesOld,
+                    particlesNewLocal);
+
+    MPI_Gather(&particlesNewLocal[0], totalParticles / p, mpi_Particle,
+               &particlesNew[0], totalParticles / p, mpi_Particle, 0,
+               MPI_COMM_WORLD);
+
+    // printf(
+    //   "Rank %d - particlesNew[0].position.x: %f, particlesNew[0].position.y: %f, particlesNew[0].radius: %d\n",
+    //   my_rank,
+    //   particlesNew[0].position.x,
+    //   particlesNew[0].position.y,
+    //   particlesNew[0].radius);
+    if (my_rank == 0) {
+      swap(*particlesOld, *particlesNew);
+      if (i % numSteps == 0) {
+        // printf("Rank %d - i: %d\n", my_rank, i);
+        imgInit(height, width, image);
+        imgUpdate(particlesOld, totalParticles, image, height, width);
+        int imageNumber  = i / numSteps;
+        string imageName = argv[9];
+        string extention = ".bmp";
+        imageName += "_";
+        imageName += to_string(imageNumber);
+        imageName += extention;
+        saveBMP(imageName.c_str(), image, width, height);
+      }
+    }
+  }
+
+  // free(image);
+
+  MPI_Finalize();
+  return 0;
+}
+
+void particlesInit(int                startIndex,
+                   int                endIndex,
+                   int                width,
+                   int                height,
+                   Particle          *particles,
+                   ParticleProperties particleProperties) {
+  double range = particleProperties.velocityMax -
+                 particleProperties.velocityMin;
+  double massRange = particleProperties.massMax - particleProperties.massMax;
+
+  for (int i = startIndex; i < endIndex; i++) {
+    double x = drand48() * width;
+    double y = drand48() * height;
+    // double z = drand48();
+    double z = 0;
+    particles[i].position = vec3(x, y, z);
+
+    double vx = (drand48() * range) + particleProperties.velocityMin;
+    double vy = (drand48() * range) + particleProperties.velocityMin;
+    // double vz = drand48();
+    double vz = 0;
+    particles[i].velocity = vec3(vx, vy, vz);
+
+    particles[i].mass = (drand48() * massRange) +
+                        particleProperties.massMax;
+
+    particles[i].colour = particleProperties.colour;
+
+    particles[i].radius = particleRadius;
+    // printf("Rank %d - i: %d, x: %f, y: %f, r: %d\n",
+    //        my_rank,
+    //        i,
+    //        particles[i].position.x,
+    //        particles[i].position.y,
+    //        particles[i].radius);
+  }
+}
+
+void updateParticles(int       numParticles,
+                     int       h,
+                     Particle *oldParticles,
+                     Particle *newParticles) {
+  int startIndex = my_rank * (numParticles / p);
+  int endIndex   = startIndex + (numParticles / p);
+  int i          = 0;
+
+  // Basic alogirthm
+  // printf("startIndex: %d, endIndex: %d\n", startIndex, endIndex);
+  for (int q = startIndex; q < endIndex; q++) {
+    // for each other particle that is not q in the same N particles
+    for (int k = 0; k < numParticles; k++) {
+      if (k != q) {
+        vec3 diff = oldParticles[q].position - oldParticles[k].position;
+        oldParticles[q].force += diff *
+                                 (oldParticles[k].mass / diff.Magnitude());
+      }
+    }
+    oldParticles[q].force = oldParticles[q].force * (-G * oldParticles[q].mass);
+
+    newParticles[i].velocity = oldParticles[q].velocity +
+                               ((oldParticles[q].force * h) /
+                                oldParticles[q].mass);
+
+    newParticles[i].position = oldParticles[q].position +
+                               (oldParticles[q].velocity * h);
+
+    newParticles[i].colour = oldParticles[q].colour;
+
+    newParticles[i].radius = oldParticles[q].radius;
+
+    // printf("Rank %d (newParticles position) - x: %f, y: %f, z: %f\n",
+    //        my_rank,
+    //        newParticles[i].position.x,
+    //        newParticles[i].position.y,
+    //        newParticles[i].position.z);
+    i++;
+  }
+}
+
+void imgInit(int            h,
+             int            w,
+             unsigned char *img) {
+  for (int j = 0; j < h; j++) {
+    for (int i = 0; i < w; i++) {
+      img[(j * w + i) * 3 + 0] = 0;
+      img[(j * w + i) * 3 + 1] = 0;
+      img[(j * w + i) * 3 + 2] = 0;
+    }
+  }
+}
+
+void imgUpdate(Particle const *particles,
+               int             numParticles,
+               unsigned char  *img,
+               int             height,
+               int             width) {
+  for (int q = 0; q < numParticles; q++) {
+    int y    =  particles[q].position.y - particles[q].radius;
+    int yEnd =  particles[q].position.y + particles[q].radius;
+    int x    =  particles[q].position.x - particles[q].radius;
+    int xEnd =  particles[q].position.x + particles[q].radius;
+    int r    = particles[q].radius;
+
+    for (int h = y; h <= yEnd; h++) {
+      for (int w = x; w <= xEnd; w++) {
+        if ((h < 0) || (h >= height)) continue;
+        if ((w < 0) || (w >= width)) continue;
+        int dx = particles[q].position.x - w;
+        int dy = particles[q].position.y - h;
+        if ((dx * dx + dy * dy) > (r * r)) continue;
+        putpixel(w, h, img, width, particles[q].colour);
+      }
+    }
+  }
+}
+
+void putpixel(int x, int y, unsigned char  *img, int width, vec3 colour) {
+  long rIndex, gIndex, bIndex;
+
+  // Get the RGB indexes
+  rIndex = (y * width + x) * 3 + 0;
+  gIndex = (y * width + x) * 3 + 1;
+  bIndex = (y * width + x) * 3 + 2;
+
+  img[rIndex] = colour.z * 255;
+  img[gIndex] = colour.y * 255;
+  img[bIndex] = colour.x * 255;
 }
--- vector3d.h
+++ vector3d.h
@@ -3,138 +3,143 @@
 
 #include <cmath>
 
-class vec3
-{
+class vec3 {
 public:
-	// Data
-	double x, y, z;
 
-	// Ctors
-	vec3( double InX, double InY, double InZ ) : x( InX ), y( InY ), z( InZ )
-		{
-		}
-	void SetDoublePoint( const double *v ) {  x=v[0]; y=v[1]; z=v[2]; }
+  // Data
+  double x, y, z;
 
-	vec3( ) : x(0), y(0), z(0)
-		{
-		}
+  // Ctors
+  vec3(double InX, double InY, double InZ) : x(InX), y(InY), z(InZ)
+  {}
 
-	// Operator Overloads
-	inline bool operator== (const vec3& V2) const 
-		{
-		return (x == V2.x && y == V2.y && z == V2.z);
-		}
+  void SetDoublePoint(const double *v) {
+    x = v[0]; y = v[1]; z = v[2];
+  }
 
-	inline vec3 operator+ (const vec3& V2) const 
-		{
-		return vec3( x + V2.x,  y + V2.y,  z + V2.z);
-		}
-	inline vec3 operator- (const vec3& V2) const
-		{
-		return vec3( x - V2.x,  y - V2.y,  z - V2.z);
-		}
-	inline vec3 SubP(const double *v) const
-		{
-		  return vec3( x - v[0],  y - v[1],  z - v[2]);
-		}
+  vec3() : x(0), y(0), z(0)
+  {}
 
-	inline vec3 operator- ( ) const
-		{
-		return vec3(-x, -y, -z);
-		}
+  // Operator Overloads
+  inline bool operator==(const vec3& V2) const
+  {
+    return x == V2.x && y == V2.y && z == V2.z;
+  }
 
-	inline vec3 operator/ (double S ) const
-		{
-		double fInv = 1.0 / S;
-		return vec3 (x * fInv , y * fInv, z * fInv);
-		}
-	inline vec3 operator/ (const vec3& V2) const
-		{
-		return vec3 (x / V2.x,  y / V2.y,  z / V2.z);
-		}
-	inline vec3 operator* (const vec3& V2) const
-		{
-		return vec3 (x * V2.x,  y * V2.y,  z * V2.z);
-		}
-	inline vec3 operator* (double S) const
-		{
-		return vec3 (x * S,  y * S,  z * S);
-		}
-	inline vec3 operator+ (double S) const
-		{
-		return vec3 (x + S,  y + S,  z + S);
-		}
-	inline vec3 operator- (double S) const
-		{
-		return vec3 (x - S,  y - S,  z - S);
-		}
+  inline vec3 operator+(const vec3& V2) const
+  {
+    return vec3(x + V2.x,  y + V2.y,  z + V2.z);
+  }
 
-	inline void operator+= ( const vec3& V2 )
-		{
-		x += V2.x;
-		y += V2.y;
-		z += V2.z;
-		}
-	inline void operator-= ( const vec3& V2 )
-		{
-		x -= V2.x;
-		y -= V2.y;
-		z -= V2.z;
-		}
+  inline vec3 operator-(const vec3& V2) const
+  {
+    return vec3(x - V2.x,  y - V2.y,  z - V2.z);
+  }
 
-	inline double operator[] ( int i )
-		{
-		if ( i == 0 ) return x;
-		else if ( i == 1 ) return y;
-		else return z;
-		}
+  inline vec3 SubP(const double *v) const
+  {
+    return vec3(x - v[0],  y - v[1],  z - v[2]);
+  }
 
-	// Functions
-	inline double Dot( const vec3 &V1 ) const
-		{
-		return V1.x*x + V1.y*y + V1.z*z;
-		}
+  inline vec3 operator-() const
+  {
+    return vec3(-x, -y, -z);
+  }
 
-	// These require math.h for the sqrt function
-	double Magnitude( ) const
-		{
-		return sqrt( x*x + y*y + z*z );
-		}
+  inline vec3 operator/(double S) const
+  {
+    double fInv = 1.0 / S;
 
-	inline void Normalize()
-		{
-		double fMag = ( x*x + y*y + z*z );
-		if (fMag == 0) {return;}
+    return vec3(x * fInv, y * fInv, z * fInv);
+  }
 
-		double fMult = 1.0/sqrt(fMag);            
-		x *= fMult;
-		y *= fMult;
-		z *= fMult;
-		return;
-		}
+  inline vec3 operator/(const vec3& V2) const
+  {
+    return vec3(x / V2.x,  y / V2.y,  z / V2.z);
+  }
+
+  inline vec3 operator*(const vec3& V2) const
+  {
+    return vec3(x * V2.x,  y * V2.y,  z * V2.z);
+  }
+
+  inline vec3 operator*(double S) const
+  {
+    return vec3(x * S,  y * S,  z * S);
+  }
+
+  inline vec3 operator+(double S) const
+  {
+    return vec3(x + S,  y + S,  z + S);
+  }
+
+  inline vec3 operator-(double S) const
+  {
+    return vec3(x - S,  y - S,  z - S);
+  }
+
+  inline void operator+=(const vec3& V2)
+  {
+    x += V2.x;
+    y += V2.y;
+    z += V2.z;
+  }
+
+  inline void operator-=(const vec3& V2)
+  {
+    x -= V2.x;
+    y -= V2.y;
+    z -= V2.z;
+  }
+
+  inline double operator[](int i)
+  {
+    if (i == 0) return x;
+    else if (i == 1) return y;
+    else return z;
+  }
+
+  // Functions
+  inline double Dot(const vec3& V1) const
+  {
+    return V1.x * x + V1.y * y + V1.z * z;
+  }
+
+  // These require math.h for the sqrt function
+  double Magnitude() const
+  {
+    return sqrt(x * x + y * y + z * z);
+  }
+
+  inline void Normalize()
+  {
+    double fMag = (x * x + y * y + z * z);
+
+    if (fMag == 0) return;
+    double fMult = 1.0 / sqrt(fMag);
+    x *= fMult;
+    y *= fMult;
+    z *= fMult;
+  }
 };
 
 inline vec3 SubtractDoubleDouble(const double *d1, const double *d2)
 {
-  return vec3(d1[0]-d2[0], d1[1]-d2[1], d1[2]-d2[2]);
+  return vec3(d1[0] - d2[0], d1[1] - d2[1], d1[2] - d2[2]);
 }
 
 inline double clamp(double d, double min, double max)
 {
-  if (d < min)
-    return min;
-  if (d > max)
-    return max;
+  if (d < min) return min;
+  if (d > max) return max;
   return d;
 }
 
-inline void clamp(vec3 &v, double min, double max) 
+inline void clamp(vec3& v, double min, double max)
 {
-  v.x = clamp(v.x,min,max);
-  v.y = clamp(v.y,min,max);
-  v.z = clamp(v.z,min,max);
+  v.x = clamp(v.x, min, max);
+  v.y = clamp(v.y, min, max);
+  v.z = clamp(v.z, min, max);
 }
 
-
-
-#endif
+#endif // ifndef vec3_h
