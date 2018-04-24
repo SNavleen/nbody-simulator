7663188ecadbdc829176fa6abfeb186b0fd63543
Navleen <navleen95@gmail.com>
2018-04-24 11:13:24 -0400

    updated the run file

--- run.sh
+++ run.sh
@@ -4,9 +4,9 @@ make
 # mpirun -np numberOfProcessors ./x.project numParticlesLight numParticleMedium numParticleHeavy numSteps subSteps timeSubStep imageWidth imageHeight imageFilenamePrex
 
 # mpirun -np 6 ./x.project 6000 6000 6000 14400 10 0.016 2550 1440 video
-mpirun -np 1 ./x.project 250 100 150 14400 10 0.016 2550 1440 video1
-mpirun -np 2 ./x.project 1000 500 500 14400 10 0.016 2550 1440 video2
-mpirun -np 4 ./x.project 2000 1000 1000 14400 10 0.016 2550 1440 video3
-mpirun -np 8 ./x.project 1000 4000 3000 14400 10 0.016 2550 1440 video3
-mpirun -np 16 ./x.project 6000 4000 5000 14400 10 0.016 2550 1440 video4
-mpirun -np 32 ./x.project 12000 10000 10000 14400 10 0.016 2550 1440 video5
+mpirun -np 1 ./x.project 250 100 150 14400 10 0.016 2550 1440 video
+mpirun -np 2 ./x.project 1000 500 500 14400 10 0.016 2550 1440 video
+mpirun -np 4 ./x.project 2000 1000 1000 14400 10 0.016 2550 1440 video
+mpirun -np 8 ./x.project 1000 4000 3000 14400 10 0.016 2550 1440 video
+mpirun -np 16 ./x.project 6000 4000 5000 14400 10 0.016 2550 1440 video
+mpirun -np 32 ./x.project 12000 10000 10000 14400 10 0.016 2550 1440 video
